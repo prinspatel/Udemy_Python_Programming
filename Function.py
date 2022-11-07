010cb25ffcd5f3396330666a547b9088c5f2d0ad
prinspatel <prince.526237@gmail.com>
2022-11-07 11:29:39 -0800

    New file Function

---
+++ Function.py
@@ -0,0 +1,20 @@
+#in Python function is group of related statments that perform a specific task.
+#function declaration
+def GreetMe():  #function start with def and after name of that function.
+    print("Good Morning")   #code
+
+GreetMe()   #call fucntion.
+
+print("------------  New Function  ------------------")
+#sand peramiter in the function.
+def morning(name):
+    print("Good Morning"+name)
+
+morning(" Prins")   #prins sand to function(morning) peramiter(name) and print usint + operator.
+print("------------  New Function  ------------------")
+#function use to sum of 2 values
+def total(a,b): #peramiter use to store value
+    print(a+b) #sum of two value
+#call the function
+total(2,3) #pass value for sum
+
