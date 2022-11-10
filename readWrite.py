ce954ffe81c65e84edfc3b76eb7bb2175c342c08
prinspatel <117322509+prinspatel@users.noreply.github.com>
2022-11-09 23:50:13 -0500

    read and write file

--- readWrite.py
+++ readWrite.py
@@ -1,16 +1,24 @@
+'''
 file = open('text_file.txt') #file name
 #print(file.read()) #read whole file
 #print(file.readline()) #read one single line at a time
-'''
+
 #read file using while loop
 line = file.readline()
 while line !="":
     print(line)
     line = file.readline()
-'''
 
 #Using readlines store file data as a list.
 for list in file.readlines():
     print(list)
 
 file.close() #close file
+'''
+
+with open('text_file.txt','r') as read:
+    content = read.readlines()
+    reversed(content)
+    with open('text_file.txt','w') as writer:
+        for line in reversed(content):
+            writer.write(line)
